896dada9f4a6fbef052f57ac7c9a4b26d858c140
naosz <aronfabian77@gmail.com>
2017-07-31 00:11:06 +0200

    testing github commit, from pycharm.

--- iris_predicition.py
+++ iris_predicition.py
@@ -1,2 +1,36 @@
-print("hello")
-print("edited, test commit")
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
+iris = load_iris()
+
+#training feature
+print(iris.data)
+
+#training target / results
+print(iris.target)
+
+
+X = iris.data
+y = iris.target
+
+print(X.shape)
+print(y.shape)
+
+#define the model
+knn = KNeighborsClassifier(n_neighbors=1)
+
+#print the model parameters
+print(knn)
+
+
+#train
+knn.fit(X,y)
+
+#create new test list
+X_new = [[3, 5, 4, 2], [5, 4, 3, 2]]
+
+#predict 2nd sample
+second_test = knn.predict(X_new)
+print(second_test)
