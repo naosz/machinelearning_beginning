471f0c9c75b9513532b1bd5a93515be7543f5989
naosz <aronfabian77@gmail.com>
2017-07-31 23:41:16 +0200

    adding LogisticRegression model.

--- iris_predicition.py
+++ iris_predicition.py
@@ -2,28 +2,16 @@ from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 
-
 iris = load_iris()
-
-#training feature
-print(iris.data)
-
-#training target / results
-print(iris.target)
-
 
 X = iris.data
 y = iris.target
-
-print(X.shape)
-print(y.shape)
 
 #define the model
 knn = KNeighborsClassifier(n_neighbors=1)
 
 #print the model parameters
 print(knn)
-
 
 #train
 knn.fit(X,y)
@@ -43,3 +31,5 @@ logreg.fit(X,y)
 y_pred = logreg.predict(X)
 print(len(y_pred))
 
+second_test_logreg = logreg.predict(X_new)
+print(second_test_logreg)
