ed3d3a71f71c1b1c18832a76eaed9f7e3a89b494
Aron Fabian <aron.fabian@blackrock.com>
2017-08-04 19:19:55 +0200

    added average for the multiple run of different K values.

--- iris_knn_best.py
+++ iris_knn_best.py
@@ -3,6 +3,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import numpy
 
 def split():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
@@ -15,19 +16,25 @@ y = iris.target
 
 result = []
 
-for k in range(0,20):
+for k in range(0,2):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
     accuracy = []
-    for i in range(1, 50):
+    for i in range(1, 5):
         knn = KNeighborsClassifier(n_neighbors= i)
         knn.fit(X_train, y_train)
         y_predict = knn.predict(X_test)
         accuracy.append(metrics.accuracy_score(y_test, y_predict))
     result.append(accuracy)
 
+print(result)
 
-for k in range(0,20):
-    plt.plot(range(1,50),result[k])
+averg = numpy.mean(result,axis=0)
+
+print(averg)
+for k in range(0,2):
+    plt.plot(range(1,5),result[k])
+plt.plot(range(1,5),averg)
 plt.xlabel("K values")
 plt.ylabel("Accuracy")
 plt.show()
+
