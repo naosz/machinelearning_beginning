c7d5845bbffad7206e83c7e424f531e26467ec22
Aron Fabian <aronfabian77@gmail.com>
2017-08-09 20:44:22 +0000

    simple regression modell to predict diabetes

---
+++ regressiondemo.py
@@ -0,0 +1,43 @@
+#learning to us regression modell
+#first thing i realized is i dont know shit about regression, time for youtube
+import  matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+diabetes = datasets.load_diabetes()
+diabetes_X = diabetes.data[:, np.newaxis, 2]
+
+#this is a retarded way to split the sample into test and train, so i made it with a function i learned form knn.
+'''
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
+
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test = diabetes.target[-20:]
+'''
+
+diabetes_X_train, diabetes_X_test, diabetes_y_train, diabetes_y_test = train_test_split(diabetes_X, diabetes.target, test_size=0.3)
+
+regr = linear_model.LinearRegression()
+
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+print("Coefficient: \n", regr.coef_)
+print("Mean squared error: %.2f"
+      % np.mean((regr.predict(diabetes_X_test) - diabetes_y_test) ** 2))
+
+print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
+
+plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+plt.plot(diabetes_X_test, regr.predict(diabetes_X_test), color='blue',
+         linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.savefig('display.png')
+
