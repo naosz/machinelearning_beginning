b566ce18765927cdcb85108b5107a76ddf72695f
naosz <aronfabian77@gmail.com>
2017-08-19 00:22:39 +0200

    added multiple runs for spliting up the sample and run regression for it.

--- regressiondemo.py
+++ regressiondemo.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn import datasets, linear_model
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from cycler import cycler
 
 diabetes = datasets.load_diabetes()
 diabetes_X = diabetes.data[:, np.newaxis, 2]
@@ -20,24 +21,29 @@ diabetes_y_train = diabetes.target[:-20]
 diabetes_y_test = diabetes.target[-20:]
 '''
 
-diabetes_X_train, diabetes_X_test, diabetes_y_train, diabetes_y_test = train_test_split(diabetes_X, diabetes.target, test_size=0.3)
 
-regr = linear_model.LinearRegression()
 
-regr.fit(diabetes_X_train, diabetes_y_train)
+for i in range(0,10):
+    diabetes_X_train, diabetes_X_test, diabetes_y_train, diabetes_y_test = train_test_split(diabetes_X, diabetes.target, test_size=0.3)
 
-print("Coefficient: \n", regr.coef_)
-print("Mean squared error: %.2f"
-      % np.mean((regr.predict(diabetes_X_test) - diabetes_y_test) ** 2))
+    regr = linear_model.LinearRegression()
 
-print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
+    regr.fit(diabetes_X_train, diabetes_y_train)
 
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, regr.predict(diabetes_X_test), color='blue',
-         linewidth=3)
+    print("Coefficient: \n", regr.coef_)
+    print("Mean squared error: %.2f"
+          % np.mean((regr.predict(diabetes_X_test) - diabetes_y_test) ** 2))
 
+    print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
+
+    plt.scatter(diabetes_X_test, diabetes_y_test)
+    plt.plot(diabetes_X_test, regr.predict(diabetes_X_test))
+
+plt.rc('lines', linewidth=1)
+plt.rc('axes', prop_cycle=(cycler('color', ['r', 'g', 'b', 'y'])))
 plt.xticks(())
 plt.yticks(())
+plt.show()
 
 plt.savefig('display.png')
 
